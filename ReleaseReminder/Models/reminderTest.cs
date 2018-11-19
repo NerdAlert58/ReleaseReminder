72c287d6fd977f756d1cd8eeae06f5a552168e57
unknown <warrensinor1616@gmail.com>
2018-11-19 13:41:31 -0600

    Scott is forcing me to commit please help

---
+++ ReleaseReminder/Models/reminderTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReleaseReminder.Models
+{
+    public class reminderTest
+    {
+       public string name { get; set; }
+       public string category { get; set; }
+        public string genre { get; set; }
+        public DateTime releaseDate { get; set; }
+    }
+}

e1631a07f8ed7d8481bc4fe6f6ed6175338c1e26
Scott <bushyhead@gmail.com>
2018-11-21 22:07:01 -0600

    Revert "Changed colors on table, added phone, added way to get movie selection back to controller"
    This reverts commit ba0e7a716905914fe784744ae065177f7363a5ed, reversing
    changes made to 0ddcbbffa683d4160fbf9b2b9ea97ef1a538c7e2.

--- ReleaseReminder/Models/ReminderProvider.cs
+++ ReleaseReminder/Models/ReminderProvider.cs
@@ -1,22 +1,70 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace ReleaseReminder.Models
 {
-    public class ReminderProvider
+    public class ReminderProvider : IReminderProvider
     {
-        private string _filePath = null;
+        private string _filePath = "reminders.json";
         private IList<Reminder> _reminders = null;
+        private IDictionary<Category, IDictionary<Genre, IList<Reminder>>> _remindersMap = null;
 
-        public ReminderProvider(string path)
+        public ReminderProvider()
         {
-            _filePath = path ?? throw new ArgumentNullException(nameof(_filePath));
             _reminders = ReadFile(_filePath) ?? throw new ArgumentNullException(nameof(_reminders));
+            _reminders = _reminders.OrderBy(o => o.ReleaseDate).ToList();
+            _remindersMap = BuildMap() ?? throw new ArgumentNullException(nameof(_remindersMap));
         }
 
-        public IList<Reminder> ReadFile(string path)
+        public IList<Reminder> Reminders()
+        {
+            return _reminders;
+        }
+        public IDictionary<Category, IDictionary<Genre, IList<Reminder>>> RemindersMap()
+        {
+            return _remindersMap;
+        }
+
+        private IDictionary<Category, IDictionary<Genre, IList<Reminder>>> BuildMap()
+        {
+            var reminderMap = new Dictionary<Category, IDictionary<Genre, IList<Reminder>>>();
+            for (int i = 0; i < _reminders.Count; i++)
+            {
+                var reminder = _reminders[i];
+                if (!reminderMap.TryGetValue(reminder.Category, out var genreMap))
+                {
+                    genreMap = new Dictionary<Genre, IList<Reminder>>();
+                }
+
+                if (reminder.Genres == null || reminder.Genres.Count <= 0) continue;
+
+                for (int j = 0; j < reminder.Genres.Count; j++)
+                {
+                    var genre = reminder.Genres[j];
+                    if (!genreMap.TryGetValue(genre, out var reminderList))
+                    {
+                        reminderList = new List<Reminder>();
+                    }
+
+                    if (reminderList.Contains(reminder))
+                    {
+                        System.Console.WriteLine($"{reminder.Title} was already in the list!!");
+                    }
+                    else
+                    {
+                        reminderList.Add(reminder);
+                    }
+                    genreMap[genre] = reminderList;
+                }
+                reminderMap[reminder.Category] = genreMap;
+            }
+            return reminderMap;
+        }
+
+        private IList<Reminder> ReadFile(string path)
         {
             var contents = File.ReadAllText(path);
             var savedValues = JsonConvert.DeserializeObject<ReminderList>(contents);
